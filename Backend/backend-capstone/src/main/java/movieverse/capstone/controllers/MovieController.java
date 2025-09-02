b48a0115c03f3dbf455d57f02c0989b319fdacf6
iTino7 <Sabatino.b007@outlook.it>
2025-09-02 18:07:23 +0200

    Fetch esterna in MovieController

--- Backend/backend-capstone/src/main/java/movieverse/capstone/controllers/MovieController.java
+++ Backend/backend-capstone/src/main/java/movieverse/capstone/controllers/MovieController.java
@@ -1,4 +1,20 @@
 package movieverse.capstone.controllers;
 
+import movieverse.capstone.services.TmdbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
 public class MovieController {
+
+    @Autowired
+    private TmdbService tmdbService;
+
+    @GetMapping(value = "/card", produces = "application/json")
+    public String getCardsMovie() {
+        return tmdbService.getCardsMovie();
+    }
 }

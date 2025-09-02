6076ef267dc02904dca78c2fea1555563c380a3c
iTino7 <Sabatino.b007@outlook.it>
2025-09-02 17:17:29 +0200

    Ho creato il token in JWTTools e iniziato l'Auth service

--- Backend/backend-capstone/src/main/java/movieverse/capstone/services/AuthService.java
+++ Backend/backend-capstone/src/main/java/movieverse/capstone/services/AuthService.java
@@ -1,4 +1,17 @@
 package movieverse.capstone.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
 }

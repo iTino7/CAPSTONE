eeb19528b9dc63dbc64c9fd11cf51848e2820407
iTino7 <Sabatino.b007@outlook.it>
2025-11-20 15:31:30 +0100

    modifiche minori alla home page

--- Frontend/capstone-project/src/Components/Home/CustomAdv.tsx
+++ Frontend/capstone-project/src/Components/Home/CustomAdv.tsx
@@ -15,14 +15,24 @@ function CustomAdv() {
         <Col className="d-flex flex-column align-items-center block">
           <div className="w-100 d-flex justify-content-center align-items-center scrollText mb-4">
             <h1 className="text-white pt-5 mt-2 title w-75 text-center" style={{ maxWidth: "100%" }}>
-              <BlurText
-                text="Stories and series, anytime, anywhere."
-                delay={150}
-                animateBy="words"
-                direction="top"
-                onAnimationComplete={() => {}}
-                className="text-2xl mb-8 justify-content-center"
-              />
+              <div className="d-flex flex-column">
+                <BlurText
+                  text="Stories and series,"
+                  delay={150}
+                  animateBy="words"
+                  direction="top"
+                  onAnimationComplete={() => {}}
+                  className="text-2xl mb-0 justify-content-center"
+                />
+                <BlurText
+                  text="anytime, anywhere."
+                  delay={200}
+                  animateBy="words"
+                  direction="top"
+                  onAnimationComplete={() => {}}
+                  className="text-2xl mb-8 justify-content-center"
+                />
+              </div>
             </h1>
           </div>
           <div className="text-center backgroundImageAdv w-75 mx-auto mb-3"></div>
